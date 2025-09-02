a6d669b0128a7aa0d9a2df4a6d101265e473c5bc
Suraj Patel <surajpatelsuraj55@gmail.com>
2025-09-03 00:58:26 +0530

    feat: added new user

--- client/src/config/allowedUsers.ts
+++ client/src/config/allowedUsers.ts
@@ -18,7 +18,8 @@ export const allowedUsers: string[] = [
   "sudsrkrace@gmail.com",
   "kumarravindrasopiya@gmail.com",
   "amankumarisjha@gmail.com",
-];
+  "Sumitkumardhotre@gmail.com"
+  ];
 
 export const isUserAllowed = (email: string): boolean => {
   return allowedUsers.includes(email.toLowerCase());
